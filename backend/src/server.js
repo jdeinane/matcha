ba258691d98a2ab5d254c3169f277cd240e65389
Jennifer UBALDO <jubaldo@f1br2s8.clusters.42paris.fr>
2026-01-15 17:02:17 +0100

    edit: changed backend port

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const io = new IOServer(server, {
 
 attachSockets(io);
 
-const port = Number(process.env.PORT || 3001);
+const port = Number(process.env.PORT || 3000);
 server.listen(port, () => {
 	console.log(`✅ Backend running on http://localhost:${port} ✅`);
 });
